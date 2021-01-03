ed427eb2001902c44b571ea74bc7f153af92ec64
San-Dan <70546259+San-Dan@users.noreply.github.com>
2021-01-03 23:56:19 +0100

    project structure

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+// require __DIR__ . '/folderName/fileName.php';
+// require __DIR__ . '//.php';
+require __DIR__ . '/resources/config.php';
+require __DIR__ . '/resources/templates/header.php';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacker News</title>
+</head>
+<body>
+
+<!-- Alla posts ska hamna i ordered list, eller table, se papper>
+    <main>
+        <ol>
+            <li>
+        </ol>
+    </main>
+
+-->
+
+<?php require __DIR__ . '/templates/footer.php'; ?>
+</body>
+</html>
