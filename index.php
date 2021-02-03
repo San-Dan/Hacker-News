2c81240e2a73c58eb61ed467af518a1a16227214
San-Dan <70546259+San-Dan@users.noreply.github.com>
2021-02-03 16:32:55 +0100

    posts table

--- index.php
+++ index.php
@@ -14,15 +14,18 @@ require __DIR__ . '/views/header.php';
         <tbody>
             <tr>
                 <th>#</th>
-                <th>POSTS</th>
+                <th>User</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Votes</th>
             </tr>
             <?php foreach ($posts as $post) : ?>
                 <tr>
                 <td><?php echo $post['id']; ?></td>
                 <td><?php echo $post['author']; ?></td>
-                <td><?php echo $post['title']; ?></td>
+                <td><a href="<?php echo $post['link']; ?>"> <?php echo $post['title']; ?></a></td>
                 <td><?php echo $post['description']; ?></td>
-                <td><?php echo $post['link']; ?></td>
+                <td><?php echo $post['COMMENTS??']; ?></td>
                 <td><?php echo $post['upvotes']; ?></td>
             </tr>
             <?php endforeach; ?>
